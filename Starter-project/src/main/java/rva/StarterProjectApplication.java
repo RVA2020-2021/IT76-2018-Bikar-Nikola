e000d657b5894248ccd6ff594c04fb6b4b94a308
Nikola Bikar <nikolabikar1@gmail.com>
2021-04-10 21:06:12 +0200

    commit message

--- Starter-project/src/main/java/rva/StarterProjectApplication.java
+++ Starter-project/src/main/java/rva/StarterProjectApplication.java
@@ -10,7 +10,7 @@ public class StarterProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarterProjectApplication.class, args); 
-		//komentar
+		//komentar 1
 	}
 
 }
