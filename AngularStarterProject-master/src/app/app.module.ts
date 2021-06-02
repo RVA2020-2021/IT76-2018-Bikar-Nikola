7895dcbf61cd371ffe9f528216f39151d4b81401
karbi1 <nikolabikar1@gmail.com>
2021-06-02 10:12:45 +0200

    2.6.2021.
    pgbr

--- AngularStarterProject-master/src/app/app.module.ts
+++ AngularStarterProject-master/src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { LigaComponent } from './components/liga/liga.component';
+import { IgracComponent } from './components/igrac/igrac.component';
+import { NacionalnostComponent } from './components/nacionalnost/nacionalnost.component';
+import { TimComponent } from './components/tim/tim.component'
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent,
+    LigaComponent,
+    IgracComponent,
+    NacionalnostComponent,
+    TimComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
