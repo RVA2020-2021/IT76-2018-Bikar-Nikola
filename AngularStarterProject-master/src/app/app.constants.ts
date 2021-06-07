627fbeb097972eb91b150acbdce7fa6429136806
Nikola <nikolabikar1@gmail.com>
2021-06-07 20:10:37 +0200

    Test

--- AngularStarterProject-master/src/app/app.constants.ts
+++ AngularStarterProject-master/src/app/app.constants.ts
@@ -1,6 +1,6 @@
-export const TIM_URL = 'http://localhost:8083/tim';
-export const NACIONALNOST_URL = 'http://localhost:8083/nacionalnost';
-export const LIGA_URL = 'http://localhost:8083/liga';
+export const TIM_URL = 'https://rva-backend-bikar-nikola.herokuapp.com/tim';
+export const NACIONALNOST_URL = 'https://rva-backend-bikar-nikola.herokuapp.com/nacionalnost';
+export const LIGA_URL = 'https://rva-backend-bikar-nikola.herokuapp.com/liga';
 
-export const IGRAC_URL = 'http://localhost:8083/igrac';
-export const IGRACI_ZA_TIM_URL = 'http://localhost:8083/igracTim';
+export const IGRAC_URL = 'https://rva-backend-bikar-nikola.herokuapp.com/igrac';
+export const IGRACI_ZA_TIM_URL = 'https://rva-backend-bikar-nikola.herokuapp.com/igracTim';
